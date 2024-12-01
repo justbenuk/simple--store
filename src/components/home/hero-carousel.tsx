ee6a46d9ff1d3f6a949dec5c9fe17fad533a3d37
justbenuk <justbenuk@gmail.com>
2024-12-01 13:09:16 +0000

    added the hero section and hero carousel

--- src/components/home/hero-carousel.tsx
+++ src/components/home/hero-carousel.tsx
@@ -1,7 +1,43 @@
+import Image from "next/image";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "../ui/carousel";
+import { Card, CardContent } from "../ui/card";
+import hero1 from "../../../public/images/hero1.jpg";
+import hero2 from "../../../public/images/hero2.jpg";
+import hero3 from "../../../public/images/hero3.jpg";
+import hero4 from "../../../public/images/hero4.jpg";
+
+const carouselImages = [hero1, hero2, hero3, hero4];
+
 export default function HeroCorousel() {
   return (
-    <>
-      <h1>Hero Carousel</h1>
-    </>
+    <div className="hidden lg:block">
+      <Carousel>
+        <CarouselContent>
+          {carouselImages.map((image, idx) => {
+            return (
+              <CarouselItem key={idx}>
+                <Card>
+                  <CardContent className="p-2">
+                    <Image
+                      src={image}
+                      alt="hero"
+                      className="w-full h-[24rem] rounded-md object-cover"
+                    />
+                  </CardContent>
+                </Card>
+              </CarouselItem>
+            );
+          })}
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
+    </div>
   );
 }
